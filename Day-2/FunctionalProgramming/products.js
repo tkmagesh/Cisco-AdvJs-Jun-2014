0db4bb21d36457e9bc9b3c15c993f750635041e1
tkmagesh <tkmagesh77@gmail.com>
2014-06-12 12:11:21 +0530

    FP - GroupBy & Join

--- Day-2/FunctionalProgramming/products.js
+++ Day-2/FunctionalProgramming/products.js
@@ -48,10 +48,84 @@ function sort(list,comparer){
       }
 }
 
+function min(list,attrName){
+	var result = list[0][attrName];
+	for(var i=1;i<list.length;i++)
+		if (list[i][attrName] < result) result = list[i][attrName];
+	return result;
+}
+
+function min(list,selector){
+	var selectorFn = selector;
+	if (typeof selector === "string")
+		selectorFn = function(item){ return item[selector]};
+	var result = selectorFn(list[0]);
+	for(var i=1;i<list.length;i++)
+		if (selectorFn(list[i]) < result) result = selectorFn(list[i]);
+	return result;
+}
+
+function max(list,selector){
+	var selectorFn = selector;
+	if (typeof selector === "string")
+		selectorFn = function(item){ return item[selector]};
+	var result = selectorFn(list[0]);
+	for(var i=1;i<list.length;i++)
+		if (selectorFn(list[i]) > result) result = selectorFn(list[i]);
+	return result;
+}
+
+function filter(list,predicate){
+	var result = [];
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]))
+			result.push(list[i]);
+	return result;
+}
+
+var costlyProductCriteria = function(p){
+	return p.cost > 50;
+}
+
+function groupBy(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){return item[selector]};
+	var result = {};
+	for(var i=0;i<list.length;i++){
+		var key = selectorFn(list[i]);
+		result[key] = result[key] || [];
+		result[key].push(list[i]);
+	}
+	return result;
+}
+
 min
 max
 avg
 filter
 groupBy
+join
+
+var categories = [
+	{id : 1, name : "stationary"},
+	{id : 2, name : "grocery"}
+]
+
+function join(leftList, rightList, leftKeyAttrName, rightKeyAttrName, transformerFn){
+	var result = [];
+	for(var i=0;i<leftList.length;i++)
+		for(var j=0;j<rightList.length;j++){
+			var leftKeyValue = leftList[i][leftKeyAttrName],
+				rightKeyValue = rightList[j][rightKeyAttrName];
+			if (leftKeyValue === rightKeyValue){
+				result.push(transformerFn(leftList[i],rightList[j]));
+			}
+		}
+	return result;
+}
+
+var productsWithCategoryNames = join(products,categories,"category","id",function(p,c){
+   return {id : p.id, name : p.name, cost : p.cost, units : p.units, category : c.name};
+});
+
 
 
